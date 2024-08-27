8ca1f853b0b159162d3e9b51df415ebbdc33d888
no-hacker-man-128 <nohackerman89@proton.me>
2024-08-27 17:03:29 +0200

    use the loop until there are no disk, if there are disk keep doing the loop

--- TowerOfHanoi.cpp
+++ TowerOfHanoi.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
 using namespace std;
  
- void move(int,int,int,int);
+void move(int,int,int,int);
+
+
+void move(int count, int p1, int p2, int p3 , int p4)
+{
+    if (count > 0)
+}
