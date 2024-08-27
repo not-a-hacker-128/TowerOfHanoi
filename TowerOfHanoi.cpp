36d5dbfa4656c803f29217c1a59d8027b278f041
no-hacker-man-128 <nohackerman89@proton.me>
2024-08-27 17:14:14 +0200

    action to insert number

--- TowerOfHanoi.cpp
+++ TowerOfHanoi.cpp
@@ -3,9 +3,22 @@ using namespace std;
  
 void move(int,int,int,int);
 
+int main() {
+    int num;
+    cout << "Enter the number of disks":
+    cin >> num;
+    cout << endl;
+    move(num,1,3,2);
+    system("PAUSE");
+    return 0;
+}
 
 void move(int count, int p1, int p2, int p3 , int p4)
 {
-    if (count > 0)
-    move( count -1 )
+    if (count > 0){
+        move( count -1 , p1, p2, p3);
+        cout << "Move disk" << count << "from" << p1 << "to" << p3 << "." << endl;
+        move(count - 1,p2,p3,p1)
+    }
+
 }
